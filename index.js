6520c22519df40951bb73545890eed7ca1ae9c72
Maxime Signoret <maxime.signoret@gmail.com>
2017-05-27 11:33:36 +0200

    [sdk] feat: add profile manager

--- index.js
+++ index.js
@@ -3,11 +3,13 @@
 import Client from './lib/Client';
 import AuthenticationManager from './lib/AuthenticationManager';
 import HeaderManager from './lib/HeaderManager';
+import ProfileManager from './lib/ProfileManager';
 import SessionManager from './lib/SessionManager';
 
 export {
     Client,
     AuthenticationManager,
     HeaderManager,
+    ProfileManager,
     SessionManager
 };
